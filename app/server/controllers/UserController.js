b5cf03afd084bc82817598e3586cbfbf7354b67c
Nadav <nadavsuliman@gmail.com>
2021-09-25 18:05:24 +0300

    Adding automatic mailing when admitting users
    When admins admit users, the users get specialized emails saying that they have been accepted.
    This will let users know that they've been accepted, so they continue their application process.

--- app/server/controllers/UserController.js
+++ app/server/controllers/UserController.js
@@ -899,7 +899,7 @@ UserController.resetPassword = function(token, password, callback){
 /**
  * [ADMIN ONLY]
  *
- * Admit a user.
+ * Admit a user and send admittance email.
  * @param  {String}   userId   User id of the admit
  * @param  {String}   user     User doing the admitting
  * @param  {Function} callback args(err, user)
@@ -920,6 +920,16 @@ UserController.admitUser = function(id, user, callback){
         new: true
       },
       callback);
+
+      //Find the user by the ID and send him an email
+    User.findOne(
+      {_id: id},
+      function(err, user){
+        if (err || !user){
+          return callback(err);
+        }
+      Mailer.sendConfirmationEmail(user.email);
+    });
   });
 };
 
